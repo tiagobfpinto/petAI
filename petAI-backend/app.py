a7cf6c7d74dcd75eebb958561e2bd4546fb335fd
LEI-122713 <jaafe@iscte-iul.pt>
2025-11-18 13:36:46 +0000

    vacilo

--- petAI-backend/app.py
+++ petAI-backend/app.py
@@ -24,8 +24,10 @@ def create_app():
 
     CORS(
         app,
-        resources={r"/*": {"origins": getattr(Config, "CORS_ORIGINS", ["*"])}},
+        resources={r"/*": {"origins": "*"}},
         supports_credentials=True,
+        methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allow_headers=["Content-Type", "Authorization"],
     )
 
     db.init_app(app)
